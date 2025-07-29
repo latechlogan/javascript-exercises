31b078b42edf213841f2211ad9d89b8b954c757d
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-07-29 12:26:06 -0500

    Add js solution for 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  let newString = [];
+  if (num >= 0) {
+    for (let i = 0; i < +num; i++) {
+      newString.push(str);
+    }
+    return newString.join("");
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
