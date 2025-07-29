e246d7a5f12b96232b0cc9b439fcc84e0653dcb3
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-07-29 14:11:20 -0500

    Add js solution for 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (baseArr, ...args) {
+  args.forEach((arg) => {
+    baseArr = baseArr.filter((element) => element !== arg);
+  });
+
+  return baseArr;
 };
+// removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
 
 // Do not edit below this line
 module.exports = removeFromArray;
