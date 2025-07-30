fef0624756d6842dbe9f6df1aa13fa7669c59772
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-07-30 13:06:31 -0500

    Add js solution for 09_sumAll challenge

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let allNumbers = [];
+  let min;
+  let max;
 
+  if (!Number.isInteger(a) || a < 0 || !Number.isInteger(b) || b < 0) {
+    return "ERROR";
+  } else {
+    if (a < b) {
+      min = a;
+      max = b;
+    } else {
+      min = b;
+      max = a;
+    }
+  }
+
+  for (let i = min; i <= max; i++) {
+    allNumbers.push(i);
+  }
+  return allNumbers.reduce((total, current) => total + current);
 };
 
 // Do not edit below this line
