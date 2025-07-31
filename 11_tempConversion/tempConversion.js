2a4999b9a48ae7d66379748f604df1a904f9e884
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-07-31 15:21:45 -0500

    Add a js solution for 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempF) {
+  let newTemp = (tempF - 32) / 1.8;
+  return Math.round(newTemp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempC) {
+  let newTemp = tempC * 1.8 + 32;
+  return Math.round(newTemp * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
