48b7dac0bd620e6b268a92662aa296dd5107831d
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-08-05 14:51:36 -0500

    Add a js solution for 14_fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (target) {
+  let fib = [1];
+  let nextNumber;
 
+  if (isNaN(target) || target < 0) return "OOPS";
+  if (+target === 0) return 0;
+  if (+target === 1) return 1;
+
+  for (let i = 0; i < +target - 1; i++) {
+    nextNumber = fib.slice(-2).reduce((a, b) => a + b, 0);
+    fib.push(nextNumber);
+  }
+
+  return nextNumber;
 };
+
+fibonacci(4);
 
 // Do not edit below this line
 module.exports = fibonacci;
