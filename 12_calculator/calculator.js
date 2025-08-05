c7195650321adfc8fb6c0777bf64df6b8080b695
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-08-04 22:44:43 -0500

    Add a js solution for 12_calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, current) => {
+    return total + current;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((total, current) => {
+    return total * current;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let temp = 1;
+
+  for (let i = 1; i <= a; i++) {
+    temp *= i;
+  }
+
+  return temp;
 };
 
 // Do not edit below this line
@@ -29,5 +39,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
