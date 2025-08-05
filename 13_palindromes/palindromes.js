a7c423b5f65a6545ed8c1d2fd0176883c023b038
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-08-04 23:30:06 -0500

    Add a js solution for 13_palindromes challenge

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (testString) {
+  const regex = /^[a-z0-9]+$/;
 
+  let letters = testString
+    .toLowerCase()
+    .split("")
+    .filter((letter) => regex.test(letter));
+  let lettersReverse = letters.toReversed();
+
+  for (let i = 0; i < letters.length; i++) {
+    if (letters[i] !== lettersReverse[i]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 // Do not edit below this line
