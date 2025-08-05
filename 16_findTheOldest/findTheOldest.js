03302c0c357b68e1e94bbb78a0f4ac8047c8fbfd
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-08-05 15:39:42 -0500

    Add a js solution for 16_findTheOldest exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  const date = new Date();
+  const thisYear = date.getFullYear();
 
+  let sorted = array.sort((a, b) => {
+    a.yearOfDeath = a.yearOfDeath || thisYear;
+    b.yearOfDeath = b.yearOfDeath || thisYear;
+
+    return a.yearOfDeath - a.yearOfBirth - (b.yearOfDeath - b.yearOfBirth);
+  });
+
+  return array.pop();
 };
 
 // Do not edit below this line
